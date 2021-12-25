ea85cf07d498913a6823c1bb89e9b98e06d7c7c2
KAnanev <russrage@gmail.com>
2021-12-25 11:44:27 +0300

    add test and code collect_diff_dict, collect_dicts_on_dict

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -3,45 +3,54 @@ import sys
 from gendiff.parser import get_data, parse_args
 
 ADDED = 'added'
-NESTED = 'nested'
 REMOVED = 'removed'
 UNCHANGED = 'unchanged'
-UPDATED = 'updated'
+CHANGED = 'changed'
+
 
 def collect_diff_dict(key, old_value=None, new_value=None, status=UNCHANGED, parent=None):
-    pass
+    collect_dict = {
+        key: {
+            old_value,
+            new_value,
+            status,
+            parent,
+        }
+    }
+    return collect_dict
 
 
-def differ(key, first_value, second_value):
+def collect_dicts_on_dict(key, first_value=None, second_value=None):
+
     if first_value and second_value:
         if first_value == second_value:
-            return f'   {key}: {first_value}'
+            return collect_diff_dict(key=key, old_value=first_value)
         else:
-            return f' - {key}: {first_value}\n + {key}: {second_value}'
-    elif first_value:
-        return f' - {key}: {first_value}'
+            return collect_diff_dict(key=key, old_value=first_value, new_value=second_value, status=CHANGED)
     elif second_value:
-        return f' + {key}: {second_value}'
+        return collect_diff_dict(key=key, new_value=second_value, status=ADDED)
+    else:
+        return collect_diff_dict(key=key, old_value=first_value, status=REMOVED)
 
 
-def diff_dict(before_dict: dict, after_dict: dict) -> str:
-    keys = sorted(list(before_dict.keys() | after_dict.keys()))
-    result = '{\n'
-
-    for key in keys:
-        first_value = str(before_dict.get(key, ''))
-        second_value = str(after_dict.get(key, ''))
-        result += f'{differ(key, first_value, second_value)}\n'
-    result += '}'
-
-    return result
+# def diff_dict(before_dict: dict, after_dict: dict) -> str:
+#     keys = sorted(list(before_dict.keys() | after_dict.keys()))
+#     result = '{\n'
+#
+#     for key in keys:
+#         first_value = str(before_dict.get(key, ''))
+#         second_value = str(after_dict.get(key, ''))
+#         result += f'{differ(key, first_value, second_value)}\n'
+#     result += '}'
+#
+#     return result
 
 
 def generate_diff(before_file, after_file):
     before_dict = get_data(before_file)
     after_dict = get_data(after_file)
 
-    return diff_dict(before_dict, after_dict)
+    # return diff_dict(before_dict, after_dict)
 
 
 def main():  # pragma: no cover
--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,4 +1,4 @@
-from gendiff.scripts.gendiff import diff_dict, generate_diff, collect_diff_dict
+from gendiff.scripts.gendiff import generate_diff, collect_diff_dict, collect_dicts_on_dict
 from gendiff.parser import get_data, parse_args
 
 
@@ -8,8 +8,8 @@ def test_get_data(path_before_file, yaml_path_before_file, yml_path_after_file):
     assert isinstance(get_data(yml_path_after_file), dict)
 
 
-def test_diff_dict(answer_data, json_before_dict, json_after_dict):
-    assert diff_dict(json_before_dict, json_after_dict) == answer_data
+# def test_diff_dict(answer_data, json_before_dict, json_after_dict):
+#     assert diff_dict(json_before_dict, json_after_dict) == answer_data
 
 
 def test_generate_diff(answer_data, path_before_file, path_after_file):
@@ -22,5 +22,13 @@ def test_parser_args(path_before_file, path_after_file):
     assert parser.second_file
     assert parser.format
 
+
 def test_collect_diff_dict():
-    assert True
+    assert collect_diff_dict(key='key') == {'key': {None,None, 'unchanged', None}}
+
+
+def test_collect_dicts_on_dict():
+    assert collect_dicts_on_dict('key', 'value', 'value') == {'key': {'value', None, 'unchanged', None}}
+    assert collect_dicts_on_dict('key', 'value') == {'key': {'value', None, 'removed', None}}
+    assert collect_dicts_on_dict('key', second_value='value') == {'key': {None, 'value', 'added', None}}
+    assert collect_dicts_on_dict('key', 'value', 'an_value') == {'key': {'value', 'an_value', 'changed', None}}
