2eebac2389cc0dcb8e7cdc57b1368706f23f1009
kananev <ananevdeveloper@yandex.ru>
2022-01-08 15:19:15 +0300

    formatter not testing

--- gendiff/formatter.py
+++ gendiff/formatter.py
@@ -1,24 +1,28 @@
 import itertools
+from typing import List
 
 from gendiff.differ import CHANGED, NESTED
 
 
-def render_string(key, value, level=0) -> str:
+def render_string(key, value, level) -> str:
     operator = value['status'][1]
     indent = '  ' * level
+    # result = []
+    status = value['status']
     if value['status'] == CHANGED:
-        return f'{indent} {operator[0]} {key}: {value["value"][0]}\n' \
-               f'{indent} {operator[1]} {key}: {value["value"][1]}'
+        result = f'{indent} {operator[0]} {key}: {value["value"][0]}\n' \
+                 f'{indent} {operator[1]} {key}: {value["value"][1]}'
     elif value['status'] == NESTED:
-        return f'{indent} {operator} {key}: {render(value["value"], level + 1)}'
+        result = f'{indent} {operator} {key}: {render(value["value"], level + 1)}'
     else:
-        return f'{indent} {operator} {key}: {value["value"]}'
+        result = f'{indent} {operator} {key}: {value["value"]}'
+    return result
 
 
 def stringify(value, replacer=' ', spaces_count=1):
 
     def iter_(current_value, depth):
-        operator = current_value['status'][1]
+
         if not isinstance(current_value, dict):
             return str(current_value)
 
@@ -33,10 +37,16 @@ def stringify(value, replacer=' ', spaces_count=1):
 
     return iter_(value, 0)
 
-def render(diff_dict: dict, level) -> str:
+
+def render(diff_dict: dict, level=0) -> str:
     """Функция форматирования словаря в строку"""
-    for key, value in diff_dict.items():
-        lines.append(render_string(key, value, level))
+    # result = []
+    result = [render_string(key, value, level) for key, value in diff_dict.items()]
+
+    # for key, value in diff_dict.items():
+    #     result.append(render_string(key, value, level))
+
+    return '\n'.join(result)
 
 
 def traverser_dict(data: dict, level=1):
--- tests/test_formatter.py
+++ tests/test_formatter.py
@@ -1,15 +1,23 @@
 from gendiff.formatter import render_string, render
-from tests.fixtures.output import output_plain
+from tests.fixtures.output import output_plain, output_nested
 
 
 def test_render_string(collect_diff_dict):
-    assert render_string('host', collect_diff_dict['host']) == '   host: hexlet.io'
-    assert render_string('host', collect_diff_dict['host']) != ' - host: hexlet.io'
-    assert render_string('timeout', collect_diff_dict['timeout']) == ' - timeout: 50\n + timeout: 20'
-    assert render_string(
+    assert render_string('host', collect_diff_dict['host'], 0) == '   host: hexlet.io'
+    assert render_string('host', collect_diff_dict['host'], 0) != ' - host: hexlet.io'
+    assert render_string('timeout', collect_diff_dict['timeout'], 0) == ' - timeout: 50\n + timeout: 20'
+    # assert render_string(
+    #     'common', {'status': ('nested', ' '), 'value': {'follow': {'status': ('added', '+'), 'value': 'false'}}}
+    #     , 0) == "   common: ['   + follow: false']"
+
+    print(render_string(
         'common', {'status': ('nested', ' '), 'value': {'follow': {'status': ('added', '+'), 'value': 'false'}}}
-    ) == '{\n   common: {\n + follow: false\n}\n)'
+        , 0))
 
 
-def test_render(collect_diff_dict):
+def test_plain_render(collect_diff_dict):
     assert render(collect_diff_dict) == output_plain
+
+
+def test_nested_render(collect_nested_diff_dict):
+    assert render(collect_nested_diff_dict) == output_nested
