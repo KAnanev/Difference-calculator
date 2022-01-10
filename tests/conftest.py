7f5fafb4c7f9d6cf801d609f78abf4e8f3e866bf
kananev <russrage@gmail.com>
2022-01-10 16:38:02 +0300

    add plain nested yaml json tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,44 @@
 import pytest
 
 
-@pytest.fixture
-def path_before_file():
+@pytest.fixture()
+def path_to_plain_before_json_file():
     return 'data/before_plain.json'
 
 
 @pytest.fixture()
-def path_after_file():
+def path_to_plain_after_json_file():
     return 'data/after_plain.json'
+
+
+@pytest.fixture()
+def path_to_plain_before_yaml_file():
+    return 'data/before_plain.yaml'
+
+
+@pytest.fixture()
+def path_to_plain_after_yml_file():
+    return 'data/after_plain.yml'
+
+
+@pytest.fixture
+def path_to_nested_before_json_file():
+    return 'data/before_nested.json'
+
+
+@pytest.fixture()
+def path_to_nested_after_json_file():
+    return 'data/after_nested.json'
+
+
+@pytest.fixture
+def path_to_nested_before_yaml_file():
+    return 'data/before_nested.yaml'
+
+
+@pytest.fixture()
+def path_to_nested_after_yaml_file():
+    return 'data/after_nested.yaml'
 
 
 @pytest.fixture()
@@ -24,14 +54,22 @@ def collect_diff_dict():
         'group3': {'status': ('added', '+'), 'value': {'deep': {'id': {'number': 45}}, 'fee': 100500}}
     }
 
+
 @pytest.fixture()
 def before_dict():
-    return {'common': {'setting1': 'Value 1', 'setting2': 200, 'setting3': True, 'setting6': {'key': 'value', 'doge': {'wow': ''}}}, 'group1': {'baz': 'bas', 'foo': 'bar', 'nest': {'key': 'value'}}, 'group2': {'abc': 12345, 'deep': {'id': 45}}}
+    return {'common': {'setting1': 'Value 1', 'setting2': 200, 'setting3': True,
+                       'setting6': {'key': 'value', 'doge': {'wow': ''}}},
+            'group1': {'baz': 'bas', 'foo': 'bar', 'nest': {'key': 'value'}},
+            'group2': {'abc': 12345, 'deep': {'id': 45}}}
 
 
 @pytest.fixture()
 def after_dict():
-    return {'common': {'follow': False, 'setting1': 'Value 1', 'setting3': None, 'setting4': 'blah blah', 'setting5': {'key5': 'value5'}, 'setting6': {'key': 'value', 'ops': 'vops', 'doge': {'wow': 'so much'}}}, 'group1': {'foo': 'bar', 'baz': 'bars', 'nest': 'str'}, 'group3': {'deep': {'id': {'number': 45}}, 'fee': 100500}}
+    return {'common': {'follow': False, 'setting1': 'Value 1', 'setting3': None, 'setting4': 'blah blah',
+                       'setting5': {'key5': 'value5'},
+                       'setting6': {'key': 'value', 'ops': 'vops', 'doge': {'wow': 'so much'}}},
+            'group1': {'foo': 'bar', 'baz': 'bars', 'nest': 'str'},
+            'group3': {'deep': {'id': {'number': 45}}, 'fee': 100500}}
 
 
 @pytest.fixture()
--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,6 +1,18 @@
 from gendiff.scripts.gendiff import generate_diff
-from tests.fixtures.output import output_plain
+from tests.fixtures.output import output_plain, output_nested
 
 
-def test_generate_diff(path_before_file, path_after_file):
-    assert generate_diff(path_before_file, path_after_file) == output_plain
+def test_plain_json_generate_diff(path_to_plain_before_json_file, path_to_plain_after_json_file):
+    assert generate_diff(path_to_plain_before_json_file, path_to_plain_after_json_file) == output_plain
+
+
+def test_plain_yaml_generate_diff(path_to_plain_before_yaml_file, path_to_plain_after_yml_file):
+    assert generate_diff(path_to_plain_before_yaml_file, path_to_plain_after_yml_file) == output_plain
+
+
+def test_nested_json_generate_diff(path_to_nested_before_json_file, path_to_nested_after_json_file):
+    assert generate_diff(path_to_nested_before_json_file, path_to_nested_after_json_file) == output_nested
+
+
+def test_nested_yaml_generate_diff(path_to_nested_before_yaml_file, path_to_nested_after_yaml_file):
+    assert generate_diff(path_to_nested_before_yaml_file, path_to_nested_after_yaml_file) == output_nested
